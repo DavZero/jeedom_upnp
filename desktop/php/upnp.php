e4d809c4908dbec24c5e32c4cc69a0f34c66e85e
DavZero <david.vinet@gmail.com>
2016-11-20 00:12:25 +0100

    Improve UI

--- desktop/php/upnp.php
+++ desktop/php/upnp.php
@@ -22,7 +22,7 @@ $eqLogics = eqLogic::byType('upnp');
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-	<legend>{{Mes equipements}}</legend>
+	<legend><i class="fa fa-table"></i>{{Mes equipements}}</legend>
 
 	<div class="eqLogicThumbnailContainer">
 		<!--<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
@@ -48,8 +48,10 @@ $eqLogics = eqLogic::byType('upnp');
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
  <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+ <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
 
    <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
     <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
     <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
   </ul>
@@ -58,8 +60,8 @@ $eqLogics = eqLogic::byType('upnp');
   <div role="tabpanel" class="tab-pane active" id="eqlogictab">
         <form class="form-horizontal">
     			<fieldset>
-    				<legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-    				<div class="form-group">
+            <br/>
+            <div class="form-group">
     					<label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
     					<div class="col-sm-3">
     						<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
