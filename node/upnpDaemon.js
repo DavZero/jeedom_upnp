804b9143bed0508a9dddd4119db34c116d32ad1c
DavZero <david.vinet@gmail.com>
2016-12-18 19:03:09 +0100

    Update daemon version

--- node/upnpDaemon.js
+++ node/upnpDaemon.js
@@ -43,7 +43,7 @@ process.argv.forEach(function (val, index, array){
 	}
 });
 
-Logger.log("Démon version 1.0.7", LogType.INFO);
+Logger.log("Démon version 1.1.0", LogType.INFO);
 Logger.log("urlJeedom = " + urlJeedom, LogType.DEBUG);
 Logger.log("serverPort = " + serverPort, LogType.DEBUG);
 Logger.log("logLevel = " + logLevel, LogType.INFO);
