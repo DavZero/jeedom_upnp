de2b42369d12d676f6092b8ac950169ae81de987
DavZero <david.vinet@gmail.com>
2017-08-24 00:14:20 +0200

    remove commented bootstrapswitch

--- desktop/js/upnp.js
+++ desktop/js/upnp.js
@@ -267,11 +267,9 @@ function addCmdToTable(_cmd) {
   tr += '</td>';
   
   tr += '<td>';
-  //tr += '<span><input type="checkbox" class="cmdAttr bootstrapSwitch" data-size="mini" data-l1key="isVisible" data-label-text="{{Afficher}}" checked/></span> ';
   tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isVisible" checked/>{{Afficher}}</label>';
   
   if (init(_cmd.type) == 'info' && (init(_cmd.subType) == 'numeric' || init(_cmd.subType) == 'binary')) {
-    //tr += '<span><input type="checkbox" class="cmdAttr bootstrapSwitch" data-size="mini" data-l1key="isHistorized" data-label-text="{{Historiser}}" /></span> ';
     tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isHistorized" checked/>{{Historiser}}</label>';
   }
   else if (init(_cmd.type) == 'action') tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="configuration" data-l2key="isOptionsVisible" checked/>{{Afficher options}}</label>';
