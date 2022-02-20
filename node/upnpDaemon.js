a1abb0c12a1bc7cbea591596e59b826338bb0279
DavZero <david.vinet@gmail.com>
2022-02-20 20:34:06 +0100

    Update for jeedom 4.2

--- node/upnpDaemon.js
+++ node/upnpDaemon.js
@@ -21,37 +21,36 @@ var disallowedList = [];
 var actionTimeout = 5;
 
 // print process.argv
-process.argv.forEach(function (val, index, array){
-	switch (index)
-	{
-	case 2:
-		urlJeedom = val;
-		break;
-	case 3:
-		serverPort = val;
-		break;
-  case 4:
-		actionTimeout = val;
-		break;
-  case 5:
-		includeState = val==1?true:false;
-		break;
-  case 6:
-    allowedList = JSON.parse(fs.readFileSync(val, 'utf8'));
-		break;
-  case 7:
-    disallowedList = JSON.parse(fs.readFileSync(val, 'utf8'));
-	case 8:
-		logLevel = val;
-		if (logLevel == 'debug')
-			Logger.setLogLevel(LogType.DEBUG);
-		else if (logLevel == 'info')
-			Logger.setLogLevel(LogType.INFO);
-		else if (logLevel == 'warning')
-			Logger.setLogLevel(LogType.WARNING);
-		else
-			Logger.setLogLevel(LogType.ERROR);
-		break;
+process.argv.forEach(function (val, index, array) {
+	switch (index) {
+		case 2:
+			urlJeedom = val;
+			break;
+		case 3:
+			serverPort = val;
+			break;
+		case 4:
+			actionTimeout = val;
+			break;
+		case 5:
+			includeState = val == 1 ? true : false;
+			break;
+		case 6:
+			allowedList = JSON.parse(fs.readFileSync(val, 'utf8'));
+			break;
+		case 7:
+			disallowedList = JSON.parse(fs.readFileSync(val, 'utf8'));
+		case 8:
+			logLevel = val;
+			if (logLevel == 'debug')
+				Logger.setLogLevel(LogType.DEBUG);
+			else if (logLevel == 'info')
+				Logger.setLogLevel(LogType.INFO);
+			else if (logLevel == 'warning')
+				Logger.setLogLevel(LogType.WARNING);
+			else
+				Logger.setLogLevel(LogType.ERROR);
+			break;
 	}
 });
 
@@ -67,66 +66,60 @@ Logger.log("disallowedList = " + JSON.stringify(disallowedList), LogType.INFO);
 /*
 if (logLevel == 'debug')
 {
-  setInterval(function(){
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    const total = process.memoryUsage().heapTotal / 1024 / 1024;
-    const rss = process.memoryUsage().rss / 1024 / 1024;
-    Logger.log(`The script uses approximately (in MB) heapUsed : ${used}, heapTotal : ${total}, rss : ${rss}`, LogType.DEBUG);
-  }, 10000);
+	setInterval(function(){
+		const used = process.memoryUsage().heapUsed / 1024 / 1024;
+		const total = process.memoryUsage().heapTotal / 1024 / 1024;
+		const rss = process.memoryUsage().rss / 1024 / 1024;
+		Logger.log(`The script uses approximately (in MB) heapUsed : ${used}, heapTotal : ${total}, rss : ${rss}`, LogType.DEBUG);
+	}, 10000);
 }
 */
 
 var busy = false;
 var jeedomSendQueue = [];
 
-var processJeedomSendQueue = function ()
-{
+var processJeedomSendQueue = function () {
 	//Logger.log('Nombre de messages en attente de traitement : ' + jeedomSendQueue.length, LogType.DEBUG);
 	var nextMessage = jeedomSendQueue.shift();
 
-	if (!nextMessage)
-	{
+	if (!nextMessage) {
 		busy = false;
 		return;
 	}
 	Logger.log('Traitement du message : ' + JSON.stringify(nextMessage), LogType.DEBUG);
 	request(
-	{
-		uri: nextMessage.url,
-    strictSSL: false,
-		qs:
 		{
-			type: nextMessage.type,
-      plugin: nextMessage.plugin
-		},
-		method: nextMessage.method,
-		json: nextMessage.data
-	}, function (err, response, body)
-	{
-		if (err)
-		{
-			Logger.log(err, LogType.WARNING);
-			if (nextMessage.tryCount < 5)
+			uri: nextMessage.url,
+			strictSSL: false,
+			qs:
 			{
-				nextMessage.tryCount++;
-				jeedomSendQueue.unshift(nextMessage);
+				type: nextMessage.type,
+				plugin: nextMessage.plugin
+			},
+			method: nextMessage.method,
+			json: nextMessage.data
+		}, function (err, response, body) {
+			if (err) {
+				Logger.log(err, LogType.WARNING);
+				if (nextMessage.tryCount < 5) {
+					nextMessage.tryCount++;
+					jeedomSendQueue.unshift(nextMessage);
+				}
+				else
+					Logger.log("Unable to send to jeedom: " + JSON.stringify(nextMessage) + ", error : " + JSON.stringify(err), LogType.ERROR);
 			}
-			else
-				Logger.log("Unable to send to jeedom: " + JSON.stringify(nextMessage) + ", error : " + JSON.stringify(err), LogType.ERROR);
+			//else Logger.log("Response from Jeedom: " + response.statusCode, LogType.DEBUG);
+			processJeedomSendQueue();
 		}
-		//else Logger.log("Response from Jeedom: " + response.statusCode, LogType.DEBUG);
-		processJeedomSendQueue();
-	}
 	);
 }
 
-var sendToJeedom = function (data, callback)
-{
+var sendToJeedom = function (data, callback) {
 	var message = {};
 	message.url = urlJeedom;
 	message.data = data;
-	message.type = 'upnp';
-  message.plugin = 'upnp';
+	message.type = 'event';
+	message.plugin = 'upnp';
 	message.method = 'POST';
 	message.tryCount = 0;
 	jeedomSendQueue.push(message);
@@ -139,186 +132,169 @@ var sendToJeedom = function (data, callback)
 var cp;
 
 //Create server for manage Jeedom->upnp
-var server = net.createServer((c) =>
-	{
-		c.on('error', (e) =>
-		{
-			Logger.log("Error server disconnected, err : " + e, LogType.WARNING);
-			//cp.shutdown();
-		}
-		);
+var server = net.createServer((c) => {
+	c.on('error', (e) => {
+		Logger.log("Error server disconnected, err : " + e, LogType.WARNING);
+		//cp.shutdown();
+	}
+	);
 
-		c.on('close', () =>
-		{
-			Logger.log("Server disconnected", LogType.DEBUG);
-			//cp.shutdown();
-		}
-		);
+	c.on('close', () => {
+		Logger.log("Server disconnected", LogType.DEBUG);
+		//cp.shutdown();
+	}
+	);
 
-		c.on('data', (data) =>
-		{
-			Logger.log("Data receive from Jeedom: " + data, LogType.DEBUG);
-			var timeoutFunction = setTimeout(function ()
+	c.on('data', (data) => {
+		Logger.log("Data receive from Jeedom: " + data, LogType.DEBUG);
+		var timeoutFunction = setTimeout(function () {
+			sendToJeedom(
 				{
-					sendToJeedom(
-					{
-						eventType: 'error',
-						description: 'Unable to process ' + data + ' within ' + actionTimeout + ' seconds'
-					}
-					);
-					try
-					{
-						c.end('Unable to process ' + data + ' within ' + actionTimeout + ' seconds');
-					}
-					catch (e)
-					{
-						//Doesn't matter if it's close the nothing else to do
-					}
-				}, actionTimeout * 1000);
-			processJeedomMessage(data, (response) =>
-			{
-				clearTimeout(timeoutFunction);
-				try
-				{
-					c.end(response);
+					eventType: 'error',
+					description: 'Unable to process ' + data + ' within ' + actionTimeout + ' seconds'
 				}
-				catch (e)
-				{
-					//Doesn't matter if it's close the nothing else to do
-				}
-			}
 			);
+			try {
+				c.end('Unable to process ' + data + ' within ' + actionTimeout + ' seconds');
+			}
+			catch (e) {
+				//Doesn't matter if it's close the nothing else to do
+			}
+		}, actionTimeout * 1000);
+		processJeedomMessage(data, (response) => {
+			clearTimeout(timeoutFunction);
+			try {
+				c.end(response);
+			}
+			catch (e) {
+				//Doesn't matter if it's close the nothing else to do
+			}
 		}
 		);
 	}
 	);
+}
+);
 
 server.listen(
-{
-	host: 'localhost',
-	port: serverPort
-}, (e) =>
-{
-	if (e != null)
-  {
-    Logger.log("Unable to start server to listen on " + serverPort + ". " + JSON.stringify(e), LogType.ERROR);
-    throw e;
-  }
-  Logger.log("Création du serveur sur le port " + serverPort, LogType.INFO);
-	Logger.log("Création du controlPoint");
-	cp = new controlPointAPI.ControlPoint(1900, allowedList, disallowedList);
-
-	cp.on('upnpError', function (err)	{
-		sendToJeedom({eventType: 'error',description: err	});
-	});
-
-	cp.on('actionDiscovered', function (action)	{
-		var data =
-		{
-			eventType: 'createAction',
-			deviceUDN: action.Service.Device.UDN,
-			serviceId: action.Service.ID,
-			name: action.Name,
-			fromDevice: action.IsFromDevice,
-			arguments: []
-		};
-		action.Arguments.forEach((item) =>{
-			if (item.Direction == 'in')
-			{
-				var argument =
-				{
-					name: item.Name,
-					type: (item.RelatedStateVariable && item.RelatedStateVariable.Type) ? item.RelatedStateVariable.Type : 'Unknow'
-				};
-
-				if (item.RelatedStateVariable && item.RelatedStateVariable.AllowedValue)
-				{
-					argument.allowedValue = item.RelatedStateVariable.AllowedValue;
-				}
-
-				data.arguments.push(argument);
-			}
-		});
-		sendToJeedom(data);
-	});
-
-	cp.on('variableDiscovered', function (variable){
-		var data =
-		{
-			eventType: 'createInfo',
-			deviceUDN: variable.Service.Device.UDN,
-			serviceId: variable.Service.ID,
-			name: variable.Name,
-			fromDevice: variable.IsFromDevice,
-			type: variable.Type
-		};
-
-		sendToJeedom(data);
-	});
-
-	cp.on('variableUpdated', function (variable, newVal){
-		var data =
-		{
-			eventType: 'updateInfo',
-			deviceUDN: variable.Service.Device.UDN,
-			serviceId: variable.Service.ID,
-			name: variable.Name,
-			fromDevice: variable.IsFromDevice,
-			type: variable.Type,
-			value: newVal
-		};
-
-		sendToJeedom(data);
-	});
-
-	cp.on('serviceUpdated', function (service){
-		var data =
-		{
-			eventType: 'updateService',
-			deviceUDN: service.Device.UDN,
-      deviceType: service.Device.Type,
-			serviceId: service.ID,
-			friendlyName: service.Device.Name,
-			location: service.Device.BaseAddress,
-			icon: service.Device.IconUrl,
-			description: service.Device.Location.href,
-      serviceDescription: service.DescriptionURL,
-			additionalData: service.Device.AdditionalInformation,
-			isOnline: true
-		};
-
-		sendToJeedom(data);
-	});
-
-	cp.on('serviceOffline', function (service) {
-		var data =
-		{
-			eventType: 'updateService',
-			deviceUDN: service.Device.UDN,
-			serviceId: service.ID,
-			isOnline: false
-		};
-		sendToJeedom(data);
-	});
-
-  //Gestion de l'inclusion
-  cp.setIncludeState(includeState);
-
-  //Search for all UPnP device
-  cp.search();
-  //For Wemo we need to perform a specific search because it doesn't respond to ssdp::all
-	cp.search('upnp:rootdevice');
-});
-
-var processJeedomMessage = function (payload, callback)
-{
-	var data = JSON.parse(payload);
-	if (data.command == 'executeAction')
 	{
+		host: 'localhost',
+		port: serverPort
+	}, (e) => {
+		if (e != null) {
+			Logger.log("Unable to start server to listen on " + serverPort + ". " + JSON.stringify(e), LogType.ERROR);
+			throw e;
+		}
+		Logger.log("Création du serveur sur le port " + serverPort, LogType.INFO);
+		Logger.log("Création du controlPoint");
+		cp = new controlPointAPI.ControlPoint(1900, allowedList, disallowedList);
+
+		cp.on('upnpError', function (err) {
+			sendToJeedom({ eventType: 'error', description: err });
+		});
+
+		cp.on('actionDiscovered', function (action) {
+			var data =
+			{
+				eventType: 'createAction',
+				deviceUDN: action.Service.Device.UDN,
+				serviceId: action.Service.ID,
+				name: action.Name,
+				fromDevice: action.IsFromDevice,
+				arguments: []
+			};
+			action.Arguments.forEach((item) => {
+				if (item.Direction == 'in') {
+					var argument =
+					{
+						name: item.Name,
+						type: (item.RelatedStateVariable && item.RelatedStateVariable.Type) ? item.RelatedStateVariable.Type : 'Unknow'
+					};
+
+					if (item.RelatedStateVariable && item.RelatedStateVariable.AllowedValue) {
+						argument.allowedValue = item.RelatedStateVariable.AllowedValue;
+					}
+
+					data.arguments.push(argument);
+				}
+			});
+			sendToJeedom(data);
+		});
+
+		cp.on('variableDiscovered', function (variable) {
+			var data =
+			{
+				eventType: 'createInfo',
+				deviceUDN: variable.Service.Device.UDN,
+				serviceId: variable.Service.ID,
+				name: variable.Name,
+				fromDevice: variable.IsFromDevice,
+				type: variable.Type
+			};
+
+			sendToJeedom(data);
+		});
+
+		cp.on('variableUpdated', function (variable, newVal) {
+			var data =
+			{
+				eventType: 'updateInfo',
+				deviceUDN: variable.Service.Device.UDN,
+				serviceId: variable.Service.ID,
+				name: variable.Name,
+				fromDevice: variable.IsFromDevice,
+				type: variable.Type,
+				value: newVal
+			};
+
+			sendToJeedom(data);
+		});
+
+		cp.on('serviceUpdated', function (service) {
+			var data =
+			{
+				eventType: 'updateService',
+				deviceUDN: service.Device.UDN,
+				deviceType: service.Device.Type,
+				serviceId: service.ID,
+				friendlyName: service.Device.Name,
+				location: service.Device.BaseAddress,
+				icon: service.Device.IconUrl,
+				description: service.Device.Location.href,
+				serviceDescription: service.DescriptionURL,
+				additionalData: service.Device.AdditionalInformation,
+				isOnline: true
+			};
+
+			sendToJeedom(data);
+		});
+
+		cp.on('serviceOffline', function (service) {
+			var data =
+			{
+				eventType: 'updateService',
+				deviceUDN: service.Device.UDN,
+				serviceId: service.ID,
+				isOnline: false
+			};
+			sendToJeedom(data);
+		});
+
+		//Gestion de l'inclusion
+		cp.setIncludeState(includeState);
+
+		//Search for all UPnP device
+		cp.search();
+		//For Wemo we need to perform a specific search because it doesn't respond to ssdp::all
+		cp.search('upnp:rootdevice');
+	});
+
+var processJeedomMessage = function (payload, callback) {
+	var data = JSON.parse(payload);
+	if (data.command == 'executeAction') {
 		Logger.log("Execution de l'action " + JSON.stringify(data));
 		var service = cp.getService(data.UDN, data.serviceID);
-		if (service == null)
-		{
+		if (service == null) {
 			Logger.log("Impossible de trouver le service " + data.UDN + "::" + data.serviceID + " sur le reseau", LogType.ERROR);
 			var message =
 			{
@@ -329,16 +305,13 @@ var processJeedomMessage = function (payload, callback)
 			if (callback)
 				callback();
 		}
-		else
-		{
-			service.executeAction(data.actionName, data.options, (err, response) =>
-			{
+		else {
+			service.executeAction(data.actionName, data.options, (err, response) => {
 				Logger.log("Envoi de la réponse " + response, LogType.DEBUG);
 				if (callback)
 					callback(response);
 
-				if (err)
-				{
+				if (err) {
 					var message =
 					{
 						eventType: 'error',
@@ -347,8 +320,7 @@ var processJeedomMessage = function (payload, callback)
 					sendToJeedom(message);
 				}
 
-				if (response)
-				{
+				if (response) {
 					var message =
 					{
 						eventType: 'updateInfo',
@@ -364,12 +336,10 @@ var processJeedomMessage = function (payload, callback)
 			);
 		}
 	}
-	else if (data.command == 'getServiceInfo')
-	{
+	else if (data.command == 'getServiceInfo') {
 		Logger.log("Mise a jour du service " + JSON.stringify(data));
 		var service = cp.getService(data.UDN, data.serviceId);
-		if (service != null)
-		{
+		if (service != null) {
 			service.updateInfo();
 			if (callback)
 				callback();
@@ -385,54 +355,46 @@ var processJeedomMessage = function (payload, callback)
 		if (callback)
 			callback();
 	}
-	else if (data.command == 'stopDaemon')
-	{
+	else if (data.command == 'stopDaemon') {
 		Logger.log("Arret du service " + JSON.stringify(data));
 		server.close();
 		cp.shutdown();
 		if (callback)
 			callback();
 	}
-  else if (data.command == 'controlPointAction')
-  {
-    if (data.subCommand == 'changeIncludeState')
-    {
-      Logger.log("Changement d'état de l'inclusion "  + data.value, LogType.INFO);
-      cp.setIncludeState(data.value?true:false);
-    }
-    else if (data.subCommand == 'scan')
-    {
-      Logger.log("Lancement d'un scan", LogType.INFO);
-      cp.search();
-      cp.search('upnp:rootdevice');
-    }
-    else if (data.subCommand == 'UpdateOnlineStatus')
-    {
-      Logger.log("Lancement de la recherche de " + data.UDN, LogType.INFO);
-      cp.checkDeviceStatus(data.UDN,(response) => {
-        if (callback) callback(response);
-      });
+	else if (data.command == 'controlPointAction') {
+		if (data.subCommand == 'changeIncludeState') {
+			Logger.log("Changement d'état de l'inclusion " + data.value, LogType.INFO);
+			cp.setIncludeState(data.value ? true : false);
+		}
+		else if (data.subCommand == 'scan') {
+			Logger.log("Lancement d'un scan", LogType.INFO);
+			cp.search();
+			cp.search('upnp:rootdevice');
+		}
+		else if (data.subCommand == 'UpdateOnlineStatus') {
+			Logger.log("Lancement de la recherche de " + data.UDN, LogType.INFO);
+			cp.checkDeviceStatus(data.UDN, (response) => {
+				if (callback) callback(response);
+			});
 			return;
-    }
-    else if (data.subCommand == 'removeService')
-    {
-      Logger.log("Suppression du service "  + data.UDN +"::"+ data.serviceId, LogType.INFO);
-      cp.removeAllowedService(data.UDN, data.serviceId);
-    }
-    else
-    {
-      var message =
-      {
-        eventType: 'error',
-        description: "Commande de controlPoint inconnu " + JSON.stringify(data)
-      };
-      sendToJeedom(message);
-    }
-    if (callback)
+		}
+		else if (data.subCommand == 'removeService') {
+			Logger.log("Suppression du service " + data.UDN + "::" + data.serviceId, LogType.INFO);
+			cp.removeAllowedService(data.UDN, data.serviceId);
+		}
+		else {
+			var message =
+			{
+				eventType: 'error',
+				description: "Commande de controlPoint inconnu " + JSON.stringify(data)
+			};
+			sendToJeedom(message);
+		}
+		if (callback)
 			callback();
-  }
-	else
-	{
+	}
+	else {
 		var message =
 		{
 			eventType: 'error',
